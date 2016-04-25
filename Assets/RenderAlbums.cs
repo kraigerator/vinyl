e28dedd51e0aeac63b2819e954be5d817947183c
Chris Millere <chris@xirvr.com>
2016-04-25 19:31:37 +0000

    Updated to final hackathon version

---
+++ Assets/RenderAlbums.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Album
+{
+	public string id { get; set; }
+	public string name { get; set; }
+	public string coverArt { get; set; }
+	//public List<Track> tracks;
+
+}
+
+public class Track {
+	public string id { get; set; }
+	public string name { get; set; }
+	public string trackName { get; set; }
+	public string discNumber { get; set; }
+	public string previewUrl { get; set; }
+}
+
+public class RenderAlbums : MonoBehaviour {
+	public GameObject albumCoverPrefab;
+	public GameObject[] albumCovers;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		string url = "https://vinyl-backend.herokuapp.com/spotify/albums";
+		WWW www = new WWW(url);
+		yield return www;
+
+		if (www.error == null)
+		{
+			Process(www.text);
+		}
+		else
+		{
+			Debug.Log("ERROR: " + www.error);
+		}
+	}
+
+	IEnumerator DownloadImage(GameObject go, string name, string url)
+	{
+		WWW www = new WWW(url);
+		yield return www;
+
+		Debug.Log (go.name);
+
+		if (go.name == name) {
+
+			go.GetComponent<Renderer> ().material.mainTexture = www.texture;
+		}
+	}
+
+	private void Process(string json)
+	{
+		Album[] albums = JsonMapper.ToObject<Album[]>(json);
+
+		if (albumCovers == null) {
+
+			albumCovers = GameObject.FindGameObjectsWithTag ("Album");
+		}
+
+		for (int i = 0; i < albums.Length; i++) {
+
+			StartCoroutine(DownloadImage(this.gameObject, albums[i].name, albums[i].coverArt));
+
+		}
+
+		Debug.Log (albumCovers.Length.ToString());
+
+
+	}
+}
---
+++ Assets/Scripts/CubeListener.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CubeListener : MonoBehaviour 
+{
+
+    public void MyPointerEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+        //this.gameObject.scene.
+        //foreach (Transform child in transform)
+        //  child.position += Vector3.up * 10.0F;
+        //transform.Rotate(new Vector3(0, 0, 0), 45);
+        // transform.Rotate(Vector3.up * 30 * Time.deltaTime);
+
+
+
+        if (this.gameObject.name.StartsWith("Kind", StringComparison.OrdinalIgnoreCase))
+        {
+            if (GameObject.Find("AudioLeft").GetComponent<AudioSource>().tag == "1")
+                return;
+            GameObject.Find("AudioLeft").GetComponent<AudioSource>().clip = Resources.Load("miles-l") as AudioClip;
+            GameObject.Find("AudioLeft").GetComponent<AudioSource>().Play();
+            GameObject.Find("AudioRight").GetComponent<AudioSource>().clip = Resources.Load("miles-r") as AudioClip;
+            GameObject.Find("AudioRight").GetComponent<AudioSource>().Play();
+
+            GameObject.Find("AudioLeft").GetComponent<AudioSource>().tag = "1";
+        }
+        else if (this.gameObject.name.StartsWith("Black", StringComparison.OrdinalIgnoreCase))
+        {
+            if (GameObject.Find("AudioLeft").GetComponent<AudioSource>().tag == "2")
+                return;
+            GameObject.Find("AudioLeft").GetComponent<AudioSource>().clip = Resources.Load("bowie-l") as AudioClip;
+            GameObject.Find("AudioLeft").GetComponent<AudioSource>().Play();
+            GameObject.Find("AudioRight").GetComponent<AudioSource>().clip = Resources.Load("bowie-r") as AudioClip;
+            GameObject.Find("AudioRight").GetComponent<AudioSource>().Play();
+
+            GameObject.Find("AudioLeft").GetComponent<AudioSource>().tag = "2";
+        }
+        else if (this.gameObject.name.StartsWith("Abstract", StringComparison.OrdinalIgnoreCase))
+        {
+            if (GameObject.Find("AudioLeft").GetComponent<AudioSource>().tag == "3")
+                return;
+            GameObject.Find("AudioLeft").GetComponent<AudioSource>().clip = Resources.Load("guides-l") as AudioClip;
+            GameObject.Find("AudioLeft").GetComponent<AudioSource>().Play();
+            GameObject.Find("AudioRight").GetComponent<AudioSource>().clip = Resources.Load("guides-r") as AudioClip;
+            GameObject.Find("AudioRight").GetComponent<AudioSource>().Play();
+
+            GameObject.Find("AudioLeft").GetComponent<AudioSource>().tag = "3";
+        }
+        else if (this.gameObject.name.StartsWith("Awake", StringComparison.OrdinalIgnoreCase))
+        {
+            if (GameObject.Find("AudioLeft").GetComponent<AudioSource>().tag == "4")
+                return;
+            GameObject.Find("AudioLeft").GetComponent<AudioSource>().clip = Resources.Load("tycho-l") as AudioClip;
+            GameObject.Find("AudioLeft").GetComponent<AudioSource>().Play();
+            GameObject.Find("AudioRight").GetComponent<AudioSource>().clip = Resources.Load("tycho-r") as AudioClip;
+            GameObject.Find("AudioRight").GetComponent<AudioSource>().Play();
+
+            GameObject.Find("AudioLeft").GetComponent<AudioSource>().tag = "4";
+        }
+
+        //transform.Rotate(Vector3.up * 60 * Time.deltaTime);
+
+        //GetComponent<AudioSource>
+    }
+
+    public void MyPointerLeave ()
+	{
+		GetComponent<Renderer>().material.color = Color.white;
+	}
+}
